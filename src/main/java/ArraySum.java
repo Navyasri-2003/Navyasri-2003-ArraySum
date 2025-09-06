c2764b12bf7dd9eab9385f59639217f2d6879004
Navyasri-2003 <navyasriap27aq8346@gmail.com>
2025-09-06 08:15:01 +0530

    Arrays

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -1,13 +1,23 @@
 public class ArraySum {
     /**
-     * This method should return the sum of all ints in an array - this is a very useful trick to know!
-     * This problem will require you to use a for loop. No other guidance will be provided.
+     * This method returns the sum of all ints in an array.
      *
      * @param arr an array of ints
-     * @return the sum of all values in arr.
+     * @return the sum of all values in arr
      */
     public int sum(int[] arr){
+        int sum = 0;
+        // Loop through the array and add each element to sum
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 
-        return 0;
+    // Optional: main method to test
+    public static void main(String[] args) {
+        ArraySum arraySum = new ArraySum();
+        int[] numbers = {1, 3, 5, 6};
+        System.out.println(arraySum.sum(numbers)); // Output: 15
     }
 }
